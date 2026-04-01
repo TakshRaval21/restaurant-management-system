981ccb9db6dbcb5286033ae7a1341e4a4dfb4e0d
TakshRaval21 <takshraval1662@gmail.com>
2026-04-01 16:28:24 +0530

    fix deploy

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -63,7 +63,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "be91d359e0d0ecdd2fac71de8c850e6e
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "6c1336986e87cf336a8f53e791d22e83",
+"flutter_bootstrap.js": "51ec23203cf2c258a4a6adb1e26ed3d0",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
